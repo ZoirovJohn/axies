4f01b09ca42729385ae54823063caa6197f140ce
Thomas <zoirovjohn@gmail.com>
2025-06-01 13:49:18 +0900

    feat: Authors Home  page  data fetching finished

--- app/components/block/TopSeller2.tsx
+++ app/components/block/TopSeller2.tsx
@@ -1,10 +1,61 @@
-"use client"
+"use client";
 import { author } from "@/data/author";
 import AuthorCard5 from "../card/AuthorCard5";
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_AGENTS } from "@/apollo/user/query";
+import { T } from "@/libs/types/common";
+import { AgentsInquiry } from "@/libs/dto/member/member.input";
+import { Member } from "@/libs/dto/member/member";
 
 export default function TopSeller2(): JSX.Element {
   const { t } = useTranslation("common");
+  const [agents, setAgents] = useState<Member[]>([]);
+  const [searchFilter, setSearchFilter] = useState<AgentsInquiry>({
+    page: 1,
+    limit: 10,
+    search: {},
+  });
+
+  // Repeat the agents to make total length exactly 10
+  const getFixedLengthAgents = (
+    list: Member[],
+    targetLength = 10
+  ): Member[] => {
+    if (!Array.isArray(list) || list.length === 0) return [];
+
+    const result: Member[] = [];
+    let i = 0;
+
+    while (result.length < targetLength) {
+      result.push(list[i % list.length]);
+      i++;
+    }
+
+    return result;
+  };
+
+  const repeatedAgents = getFixedLengthAgents(agents, 10);
+
+  /** APOLLO REQUESTS **/
+  // const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_AGENTS, {
+    fetchPolicy: "network-only",
+    variables: { input: searchFilter },
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data: T) => {
+      setAgents(data?.getAgents?.list || []);
+    },
+  });
+
+  console.log("HomePage TopSellers agents:", agents);
 
   return (
     <>
@@ -16,8 +67,8 @@ export default function TopSeller2(): JSX.Element {
             </div>
             <div className="col-md-12">
               <div className="tf-box">
-                {author.slice(0, 10).map((item) => (
-                  <AuthorCard5 key={item.id} data={item} />
+                {repeatedAgents.slice(0, 10).map((item, index) => (
+                  <AuthorCard5 key={`${item._id}-${index}`} agent={item} />
                 ))}
               </div>
             </div>
--- app/components/card/AuthorCard5.tsx
+++ app/components/card/AuthorCard5.tsx
@@ -1,11 +1,53 @@
+import { REACT_APP_API_URL } from "@/app/config";
+import { MeFollowed } from "@/libs/dto/follow/follow";
+import { MeLiked } from "@/libs/dto/property/property";
+import {
+  MemberAuthType,
+  MemberStatus,
+  MemberType,
+} from "@/libs/enums/member.enum";
 import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-  data: { id: number; avatar: string; name: string; eth: number };
+  agent: {
+    _id: string;
+    memberType: MemberType;
+    memberStatus: MemberStatus;
+    memberAuthType: MemberAuthType;
+    memberPhone: string;
+    memberNick: string;
+    memberPassword?: string;
+    memberFullName?: string;
+    memberImage?: string;
+    memberAddress?: string;
+    memberDesc?: string;
+    memberProperties: number;
+    memberRank: number;
+    memberArticles: number;
+    memberPoints: number;
+    memberLikes: number;
+    memberFollowers?: number;
+    memberFollowings?: number;
+    memberViews: number;
+    memberComments: number;
+    memberWarnings: number;
+    memberBlocks: number;
+    deletedAt?: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    // Enable for authentications
+    meLiked?: MeLiked[];
+    meFollowed?: MeFollowed[];
+    accessToken?: string;
+  };
 }
 
-export default function AuthorCard5({ data }: Props): JSX.Element {
+export default function AuthorCard5({ agent }: Props): JSX.Element {
+  const imagePath: string = agent?.memberImage
+    ? `${REACT_APP_API_URL}/${agent?.memberImage}`
+    : "/assets/images/avatar/avt-28.jpg";
+
   return (
     <>
       <div className="box-item">
@@ -15,7 +57,7 @@ export default function AuthorCard5({ data }: Props): JSX.Element {
               <Image
                 height={200}
                 width={200}
-                src={data.avatar}
+                src={imagePath}
                 alt="Image"
                 className="avatar"
               />
@@ -26,9 +68,9 @@ export default function AuthorCard5({ data }: Props): JSX.Element {
           </div>
           <div className="author-infor">
             <h5 className="fs-16">
-              <Link href="/authors-1">{data.name}</Link>
+              <Link href="/authors-1">{agent.memberNick}</Link>
             </h5>
-            <span className="price">{data.eth} ETH</span>
+            <span className="price">AGENT</span>
           </div>
         </div>
       </div>
