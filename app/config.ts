68ab3f8a3662b107c9b5a9dc54c1c3b07abb7ea4
Thomas <zoirovjohn@gmail.com>
2025-07-09 14:02:24 +0900

    fix: modify config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const REACT_APP_API_URL =
-  `${process.env.REACT_APP_API_URL}` || "http://localhost:5001";
+  `${process.env.REACT_APP_API_URL}`;
 
 export const availableOptions = ["propertyBarter", "propertyRent"];
 
